b60698e73e7f62c3e3e2e68b8226363a0d8137d0
Keertan Gir <gkeertangirgmail.com>
2026-09-05 13:57:34 +0500

    Practice the Drived States, making the

--- 04_travel_list/src/App.js
+++ 04_travel_list/src/App.js
@@ -15,20 +15,22 @@ import { useState } from "react";
 // this is the main second Parent Component after index.js .
 export default function App() {
 
-  const [items, setItems] = useState( [] );
+  const [items, setItems] = useState([]);
 
-    function handleAddItems(item){
-      setItems([...items, item]);
-    }
 
-    function handleDeleteItem(id){
-      setItems( (items) => items.filter( (item) => item.id !== id ));  
-    }
 
-    function handleToggleItem(id){
-      setItems( (items) => items.map( (item) => 
-        item.id === id ? { ...item, packed: !item.packed }  :  item) )
-    }
+  function handleAddItems(item) {
+    setItems([...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) => items.map((item) =>
+      item.id === id ? { ...item, packed: !item.packed } : item))
+  }
 
 
 
@@ -36,8 +38,8 @@ export default function App() {
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackageList items= { items}  onDeleteItem={ handleDeleteItem } onToggleItem={ handleToggleItem } />
-      <Status />
+      <PackageList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
+      <Status items={items} />
 
     </div>
   );
@@ -56,82 +58,84 @@ function Logo() {
 // this is the main component that handle the All Form and user intraction and it is responsible for the 
 // creating items and other things.
 // We can also pass the state through the props
-function Form( { onAddItems } ) {
+function Form({ onAddItems }) {
 
   // here we declare the states and uses the useState("") hook that we import from the react and destructre that
   // arry and use them in out this components
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
- 
 
 
-// this funtion is responsible for the handling the submition of the Form
+
+  // this funtion is responsible for the handling the submition of the Form
   function handleOnSubmit(e) {
-  
+
     // this is responsiple making this componet prevent from the default setting of the form.
     // like isme yeh hy k yeh page ko submission ke bad reload krne se rokta hy
-  e.preventDefault();
-
-  
-
-  // simple JS - custom validation that helps to stop the empty entries
-  if(!description) return;
-
-  // creating new object for the item
-  const newItem = { description , quantity, packed: false , id: Date.now() } 
+    e.preventDefault();
 
 
-  onAddItems(newItem);
 
-  console.log(newItem);
+    // simple JS - custom validation that helps to stop the empty entries
+    if (!description) return;
 
-  // making states on the initial states
-  setDescription("");
-  setQuantity(1);
-
-  // this the concept of the Controlled Elements in which we controls the form elements 
-
-}
+    // creating new object for the item
+    const newItem = { description, quantity, packed: false, id: Date.now() }
 
 
-// here we are returning the component in JSX format
+    onAddItems(newItem);
+
+    console.log(newItem);
+
+    // making states on the initial states
+    setDescription("");
+    setQuantity(1);
+
+    // this the concept of the Controlled Elements in which we controls the form elements 
+
+  }
+
+
+  // here we are returning the component in JSX format
   return (
-    <form className="add-form" onSubmit={ handleOnSubmit } >
+    <form className="add-form" onSubmit={handleOnSubmit} >
 
       <h3> What You need for your ✈ trip </h3>
 
       {/* controlling Elements with the use of state and the value and onchange props in these elements */}
       <select value={quantity} onChange={(e) => {
-        setQuantity( Number( e.target.value ))}}  >
+        setQuantity(Number(e.target.value))
+      }}  >
 
 
-          {/* rendring the quantity options in dunamic way */}
-        { Array.from( {length : 20}, (_, i)  => i + 1).map( num => (
-            <option value={num} key={num} > {num} </option>
+        {/* rendring the quantity options in dunamic way */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+          <option value={num} key={num} > {num} </option>
         ))}
 
       </select>
-      
+
       {/* same as upside */}
-      <input 
-      type="text" 
-      placeholder="Enter Item Name"
-      value={description}
-      onChange={ (e) => {
-        console.log(e.target.value)
-        setDescription(e.target.value)} }
-      /> 
+      <input
+        type="text"
+        placeholder="Enter Item Name"
+        value={description}
+        onChange={(e) => {
+          console.log(e.target.value)
+          setDescription(e.target.value)
+        }}
+      />
       <button>Add</button>
     </form>)
 }
 
-function PackageList( { items , onDeleteItem, onToggleItem } ) {
+function PackageList({ items, onDeleteItem, onToggleItem }) {
   return (
-//  returning the list dynamically
+    //  returning the list dynamically
     <div className="list" >
       <ul>
-        { items.map((item) => 
-        <Item item={item} key={item.id} onDeleteItem = {onDeleteItem} onToggleItem={ onToggleItem } /> )}
+        {items.map((item) =>
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />)}
       </ul>
     </div>
 
@@ -140,28 +144,49 @@ function PackageList( { items , onDeleteItem, onToggleItem } ) {
 }
 
 // Component for the items
-function Item({ item , onDeleteItem, onToggleItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li >
 
-        <input type="checkbox" value={item.packed} onClick={ () => onToggleItem(item.id) }  />
+      <input type="checkbox" value={item.packed} onClick={() => onToggleItem(item.id)} />
       {/* applying the Styling on the conditions packed or not */}
-        <span style={ 
-          item.packed ?
-           { textDecoration: "line-through" }  
-           : {} 
-           } > {item.quantity} {item.description} </span>
-        <button onClick={ () =>  onDeleteItem(item.id)} > ❌ </button>
-     </li>
-    
+      <span style={
+        item.packed ?
+          { textDecoration: "line-through" }
+          : {}
+      } > {item.quantity} {item.description} </span>
+      <button onClick={() => onDeleteItem(item.id)} > ❌ </button>
+    </li>
+
   )
 }
 
 // here is the component for the footer
-function Status() {
+function Status({ items }) {
+
+  
+  if (!items.length) {
+      return (
+        <footer className="stats" >
+          <em> Add Items In your list! ✨ </em>
+        </footer>
+      )
+  }
+  
+
+  const itemCount = items.length;
+  const packed = items.filter(item => item.packed === true).length;
+
+  const percentage = (packed / itemCount * 100);
+
   return (
     <footer className="stats" >
-      <em> You have X items on your List. and Your %X packing is Complete </em>
+      {
+        percentage === 100 ?
+          (<em> Packing is Completed, You are Ready to Go! ✈ </em>)
+          :
+          (<em> You have {itemCount} items on your List. You pack {packed} items!  Your  {percentage}% packing is Complete </em>)
+      }
     </footer>)
 
 }
